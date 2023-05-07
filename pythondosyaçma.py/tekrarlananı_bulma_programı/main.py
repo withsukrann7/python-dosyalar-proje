649860cf6e6bfa276fbcd3de40ec3b44315731fa
şükran <125204831+withsukrann7@users.noreply.github.com>
2023-05-08 02:27:13 +0300

    Update main.py

--- pythondosyaçma.py/tekrarlananı_bulma_programı/main.py
+++ pythondosyaçma.py/tekrarlananı_bulma_programı/main.py
@@ -1,35 +1,3 @@
-"""
-Soru: Bir metin dosyasında, bir kelimenin kaç kez geçtiğini sayan bir Python programı yazın.
-Programın kullanıcıdan kelimeyi girmesini isteyin ve dosyadaki kelimenin kaç kez geçtiğini gösterin.
-
-çıktısı böyle olsun: 
-
-Dosya adı: programming.txt
-Aranacak kelime: Python
-Dosyada 'Python' kelimesi 2 kez geçiyor.
-
-"""
-"""
-with open("metin.txt","r",encoding="UTF-8") as f:
-   
-
-    liste = []
-
-    name = input(str("aranacak kelimeyi yazınız:"))
-
-    for i in f:
-        if name in liste:
-            liste[name] += 1
-        else:
-            liste[name] = 1
-
-    print("toplam geçen sayı:",len(liste[name]))
-
-print("aranan kelime:",name)
-print("dosyada {} kelimesi {} kez geçiyor".format(name),(len(liste[name])))
-"""
-
-
 with open("metin.txt","r",encoding="UTF-8") as f:
 
     liste = {}
@@ -48,4 +16,3 @@ with open("metin.txt","r",encoding="UTF-8") as f:
         print("dosyada {} kelimesi {} kez geçiyor".format(name, liste[name]))
     else:
         print("dosyada {} kelimesi geçmiyor".format(name))
-
